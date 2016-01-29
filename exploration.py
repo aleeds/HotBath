3c271642d9ea08d1f94a77b267322d3b91bff4ed
Andy Leeds <aleeds444@gmail.com>
2016-01-29 13:08:45 -0600

    Added our exploration of how heat transfer works

--- exploration.py
+++ exploration.py
@@ -1,11 +1,11 @@
 def equil(t_one,t_two):
-    Q = .6 * 1 * abs(t_one - t_two) / .01
+    Q = .6 * 1 * (t_one - t_two) / .01
     # this is the same as joules, cause 1 second
     t_one_new = t_one - Q / 10000
     t_two_new = t_two + Q / 10000
     return (t_one_new,t_two_new)
 
-ls = [(1,(100,50))]
+ls = [(1,(50,100))]
 for t in range(1,1000):
     v = ls[t - 1]
     a = v[1][0]
@@ -21,12 +21,14 @@ ls_temp_b = [b for (t,(a,b)) in ls]
 
 plt.plot(ls_time,ls_temp_b)
 plt.show()
+
 def equil_two(t_one,t_two):
     Q = .6 * 1 * abs(t_one - t_two) / .01
     # this is the same as joules, cause 1 second
     t_one_new = t_one - Q / 100000
     t_two_new = t_two + Q / 100000
     return (t_one_new,t_two_new)
+
 ls = [(1,(100,50))]
 for t in range(1,1000):
     v = ls[t - 1]
